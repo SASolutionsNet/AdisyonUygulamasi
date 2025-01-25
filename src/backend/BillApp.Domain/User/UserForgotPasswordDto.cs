c056f67f8d2b1f1b0a08152e7fa3b6ecde0a045a
KhutuluN <sevda_ergun@eskisehir.edu.tr>
2025-01-25 22:39:34 +0300

    Login and other user endpoints done and tested.

---
+++ src/backend/BillApp.Domain/User/UserForgotPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillApp.Domain.User
+{
+    public class UserForgotPasswordDto
+    {
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
